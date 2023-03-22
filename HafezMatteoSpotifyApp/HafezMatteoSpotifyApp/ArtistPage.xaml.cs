b3000234a8e643bfaad5509d94fd55e7a2a5722a
Ouidkling - Laptop <matteo.nadler.campou@gmail.com>
2023-03-22 17:26:59 +0100

    ça marche de fou ! (Page artiste toutes les infos bonnes)

--- HafezMatteoSpotifyApp/HafezMatteoSpotifyApp/ArtistPage.xaml.cs
+++ HafezMatteoSpotifyApp/HafezMatteoSpotifyApp/ArtistPage.xaml.cs
@@ -19,6 +19,10 @@ namespace HafezMatteoSpotifyApp
             FullArtist _ArtisteClient = SpotifyService.Instance.GetSpotifyClient().Artists.Get("4IF11U0nzFhAaLDGZH3vSx").Result;
             this.PhotoArtiste.Source = _ArtisteClient.Images[0].Url;
             this.NomArtiste.Text = _ArtisteClient.Name;
+            this.GenreArtiste1.Text = _ArtisteClient.Genres[0];
+            this.GenreArtiste2.Text = _ArtisteClient.Genres[1];
+            this.FollowersArtiste.Text = _ArtisteClient.Followers.Total.ToString();
+            this.PopulariteArtiste.Text = _ArtisteClient.Popularity.ToString();
         }
     }
 }
