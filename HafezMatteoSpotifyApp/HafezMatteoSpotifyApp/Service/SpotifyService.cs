282ec59ccdfdedaeb256af174ee6403a24eb2bbb
Ouidkling - Laptop <matteo.nadler.campou@gmail.com>
2023-03-27 13:37:01 +0200

    Ajout des icones de la Tab Bar

--- HafezMatteoSpotifyApp/HafezMatteoSpotifyApp/Service/SpotifyService.cs
+++ HafezMatteoSpotifyApp/HafezMatteoSpotifyApp/Service/SpotifyService.cs
@@ -8,7 +8,7 @@ namespace HafezMatteoSpotifyApp.Service
 {
     public class SpotifyService
     {
-        private const string TokenSpotify = "BQDA0HBAFIjQSiK1mecqVL2WpjAOQVFPx5v4plwUbkhxFVhYZ98M8WskD5UoGlwPQcPyVgm-jFeDA-WkoA05yXjDB-1qtrDWHANBGPkee79MBsnGYnaFIC3iDPdIPcVSHrexrCC5o9L1K9YUoagb7Cwp_1Fy0l3pakulv6IYTmVWw6jUY5U1EEtV0Ahc3eGUpc8kvwSUod0IaujbZUc6tL2HKpUe_E2oAjV_rib7c7FPOMnQpF1xIZn7Mu8kYH9n_OHXBfo0Z1Z-3XITm-XV4qRUbWBc4dmGfMQaO8N6_QygxHB4hMaw";
+        private const string TokenSpotify = "BQDdBkgiIHmw0PFmE3NyKtB2GOCN8_HtBE-STovKtExUJoXthWZKWgubGYLPeuJijnWGqjsXXkrpVfM5H0azJ5K3bl-Nn3U4y3A9EqCMopZJM_u2RqJO";
         private SpotifyClient _spotifyClient;
         #region Instance
 
