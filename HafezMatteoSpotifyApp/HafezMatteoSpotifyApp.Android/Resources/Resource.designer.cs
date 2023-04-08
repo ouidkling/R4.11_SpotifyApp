2f06ec2f56f3e7af7a1dc9e0a568e0049dffb788
Ouidkling - Domicile <matteo.nadler.campou@gmail.com>
2023-04-08 21:19:03 +0200

    Ajout icones sur Android

--- HafezMatteoSpotifyApp/HafezMatteoSpotifyApp.Android/Resources/Resource.designer.cs
+++ HafezMatteoSpotifyApp/HafezMatteoSpotifyApp.Android/Resources/Resource.designer.cs
@@ -18789,6 +18789,9 @@ namespace HafezMatteoSpotifyApp.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int xamarin_spotify = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
